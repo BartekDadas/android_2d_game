43beef864cadf6396bbbb36fde9136bed9aefa84
BartekDadas <dev21maker@gmail.com>
2025-02-01 14:07:03 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/devmakersapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.devmakersapplication.app
+
+import com.devmakersapplication.app.appcomponents.base.BaseActivity
+import com.devmakersapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
